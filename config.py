93cf1bb4edf5d99016fa1489981c0524a76ead14
vusalgizli <102589016+vusalgizli@users.noreply.github.com>
2022-04-04 16:37:16 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
-OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
+API_ID = int(os.getenv("16602040"))
+API_HASH = os.getenv("a969618f49b2b2e0e33fc745e266bf17")
+BOT_TOKEN = os.getenv("5266578609:AAGWX-5owgJ9TxDyK0lz5aDK37UGJIXsdes")
+DATABASE_URL = os.getenv("mongodb+srv://aylak:aylak@cluster0.cd5my.mongodb.net/Cluster0?retryWrites=true&w=majority")
+OWNER_ID = list({int(x) for x in os.environ.get("5266934814", "").split()})
